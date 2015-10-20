f28d8e20a7679f08c17eb9bd2ebe9506940b4469
Victor Castillo <victor23@gmail.com>
2015-10-20 02:25:18 +0200

    Update msyql_backup.sh

--- msyql_backup.sh
+++ msyql_backup.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-# 2010/7/30
-# Spencer Steffen
-# Citrus Media Group
-# http://citrusme.com/
-# http://github.com/citrus/mysql-backups
+# original http://github.com/citrus/mysql-backups
 
 SCRIPT_NAME="MySQL Backups"
 VERSION="0.0.3"
 
-BACKUPS=/path/to/your/backups
+BACKUPS=/home/backups
 
 MYSQL="$(which mysql)"
 MYSQLDUMP="$(which mysqldump)"
@@ -18,7 +14,7 @@ HOST="localhost"
 USER="root"
 PASS="password"
 
-NOW=$(date +"%d%H")
+NOW="mysql"
 
 
 if [ ! -d $BACKUPS/$NOW/ ]; then
@@ -33,11 +29,11 @@ for db in $DBS; do
   
   if [ "$db" != "information_schema" ]; then
   
-    time=$(date +"%H-%M-%S")
+    time=$(date +"%d-%m-%y-%k")
     dump="$db-$time.sql"
         
     $MYSQLDUMP -h$HOST -u$USER -p$PASS $db > $BACKUPS/$NOW/$dump
-    
+    gzip -f $BACKUPS/$NOW/$dump    
     echo "$BACKUPS/$NOW/$dump saved!"
     
   fi
